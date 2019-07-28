98878374b04ce33cb5d82a7bf524287b7a8f8c16
taohaibing <binghaitao@126.com>
2019-07-28 19:22:04 +0800

    检查资源管理

--- src/const/crud/base/doctorinspectresource.js
+++ src/const/crud/base/doctorinspectresource.js
@@ -5,39 +5,68 @@ export const tableOption = {
   stripe: true,
   menuAlign: 'center',
   align: 'center',
+  labelWidth: "130",
   column: [
     {
       fixed: true,
       label: '编号',
       prop: 'inspResourceId',
-      span: 24,
       hide: true,
       editDisabled: true,
       addDisplay: false
     },
 	  {
-      label: '检查类别（体格检查，功能检查，化验检查）',
-      prop: 'hospitalName'
+      label: '检查类别',
+      prop: 'hospitalName',
+      type: "select",
+      dicData: [{
+        label: '体格检查',
+        value: 0
+      }, {
+        label: '功能检查',
+        value: 1
+      }, {
+        label: '化验检查',
+        value: 2
+      }],
     },
 	  {
-      label: '检查项目名称',
-      prop: 'inspItemName'
+      label: '检查项目',
+      prop: 'inspItemName',
+      type: 'select',
+      dicUrl: '/base/doctorinspectionitem/dict',
+      props: {
+        label: 'inspItemName',
+        value: 'inspItemId'
+      }
+
     },
 	  {
       label: '收费单价',
-      prop: 'unitPrice'
+      prop: 'unitPrice',
+      type: 'number',
+      precision:2,
     },
 	  {
       label: '数量',
-      prop: 'quantity'
+      prop: 'quantity',
+      type: 'number',
     },
 	  {
       label: '开始时间',
-      prop: 'startTime'
+      prop: 'startTime',
+      type: "datetime",
+      format: "yyyy-MM-dd hh:mm",
+      valueFormat: "timestamp",
+
     },
+
 	  {
       label: '结束时间',
-      prop: 'endTime'
+      prop: 'endTime',
+      type: "datetime",
+      format: "yyyy-MM-dd hh:mm",
+      valueFormat: "timestamp",
     },
 	  {
       label: '备注或说明',
@@ -52,7 +81,6 @@ export const tableOption = {
       valueFormat: 'yyyy-MM-dd HH:mm:ss',
       editDisabled: true,
       addDisplay: false,
-      span: 24
     }
   ]
 }
