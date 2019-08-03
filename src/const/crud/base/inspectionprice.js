8a066bbb64290956990cafe97948652edb458048
gkylin <1083584170@qq.com>
2019-08-03 15:26:18 +0800

    增加价格模块

---
+++ src/const/crud/base/inspectionprice.js
@@ -0,0 +1,26 @@
+export const tableOption = {
+  border: true,
+  index: true,
+  indexLabel: '序号',
+  stripe: true,
+  menuAlign: 'center',
+  align: 'center',
+  column: [
+	  {
+      label: '主键',
+      prop: 'inspPriceId'
+    },
+	  {
+      label: '机构名称',
+      prop: 'hospitalId'
+    },
+	  {
+      label: '项目名称',
+      prop: 'inspItemId'
+    },
+	  {
+      label: '价格',
+      prop: 'inspPrice'
+    }
+  ]
+}
