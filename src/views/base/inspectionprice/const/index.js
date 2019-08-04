3878b8a9c64a2af4a2762e5e5e501bf3d54dc8ec
taohaibing <binghaitao@126.com>
2019-08-04 10:21:40 +0800

    更新价格校验

--- src/views/base/inspectionprice/const/index.js
+++ src/views/base/inspectionprice/const/index.js
@@ -1,3 +1,17 @@
+const validatePrice  = (rule, value, callback) => {
+  console.log("validateGapTime:"+value)
+  let hour = value.split(' ')[1].split(':')[0]
+  console.log("hour:"+hour)
+  if((hour>=8 && hour<12)||(hour>=13&&hour<17))
+  {
+    console.log("hour-true:"+hour)
+    callback();
+  }else{
+    console.log("hour-false:"+hour)
+    callback(new Error('请输入正确时间段8～12 13～17'));
+  }
+};
+
 export const tableOption = {
   border: true,
   index: true,
@@ -5,17 +19,25 @@ export const tableOption = {
   stripe: true,
   menuAlign: 'center',
   align: 'center',
+  dialogWidth: '50%',
+  labelWidth: 130,
+  dialogHeight: '50%',
   column: [
 	  {
       label: '医院名称',
       prop: 'hospitalId',
       type: "select",
       dicUrl: '/base/doctorhospital/dict',
+      span: 12,
+      row:true,
       props: {
         label: 'name',
         value: 'hospitalId'
       },
       search:true,
+      rules: [
+        { required: true, message: '不能为空', trigger: 'change' },
+      ]
     },
 	  {
       label: '项目名称',
@@ -27,10 +49,22 @@ export const tableOption = {
         value: 'inspItemId'
       },
       search:true,
+      span: 12,
+      row:true,
+      rules: [
+        { required: true, message: '不能为空', trigger: 'change' },
+      ]
     },
 	  {
       label: '价格',
-      prop: 'inspPrice'
+      prop: 'inspPrice',
+      type: "number",
+      minRows: 0,
+      span: 12,
+      row:true,
+      rules: [
+        { required: true, message: '不能为空', trigger: 'change' },
+      ]
     }
   ]
 }
